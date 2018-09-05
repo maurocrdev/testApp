95380a1e812606798b91e47a17b8becde7c7f9db
mauro <maurocrdev@gmail.com>
2018-09-05 10:43:05 -0500

    Initial update

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Articles One',
+            'body':'Lorem ipsum',
+            'author':'mauro',
+            'create_date':'04-25-2017'
+
+        },
+        {
+            'id': 2,
+            'title': 'Articles Two',
+            'body': 'Lorem ipsum',
+            'author': 'lopez',
+            'create_date': '04-25-2017'
+
+        },
+        {
+            'id': 3,
+            'title': 'Articles Three',
+            'body': 'Lorem ipsum',
+            'author': 'joe',
+            'create_date': '04-25-2017'
+
+        }
+
+    ]
+    return articles
